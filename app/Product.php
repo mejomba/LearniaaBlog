46e3b2dc5478fb9b5148255180b1ba7daae75add
user <maxmoler1376@gmail.com>
2019-10-05 15:07:27 +0330

    ver 1.0 m.reza

--- app/Product.php
+++ app/Product.php
@@ -8,4 +8,20 @@ class Product extends Model
 {
     protected $table = 'products';
     protected $primaryKey = 'pk_product';
+
+
+    public function GetListAllNameColumns_ForTable()
+    {
+       return  [ 'title' => 'زمان',
+                 'pic' => 'تصویر ',
+                 'price' => 'قیمت  ',
+                 'time' => 'زمان',
+                 'desc' => 'توضیحات',
+                 'count' => 'تعداد قسمت ها',
+                 'language' => ' زبان',
+                 'subtitle' => 'زیرنویس',
+
+                 ] ;
+    }
+    
 }
--- app/Transaction.php
+++ app/Transaction.php
@@ -10,4 +10,19 @@ class Transaction extends Model
     protected $primaryKey = 'pk_transaction';
 
     protected $fillable = [ 'pk_users', 'price', 'digital_receipt','created_at','updated_at']; 
+
+
+
+    public function GetListAllNameColumns_ForTable()
+    {
+       return  [ 'time' => 'زمان',
+                 'date' => 'تاریخ ',
+                 'digital_receipt' => 'رسید دیجیتال ',
+                 'price' => 'قیمت',
+                 'type' => 'نوع',
+                 'created_at' => 'ساخته شده در',
+                 'updated_at' => 'ویرایش شده در',
+
+                 ] ;
+    }
 }
