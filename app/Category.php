318510f484b133530be3080c1f30124cb1d1202c
learniaa_Team <learniaa@gmail.com>
2019-10-05 16:09:26 +0330

    step 5.4

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-   use HasApiTokens ; 
+   
 
     protected $table = 'categories';
     protected $primaryKey = 'pk_categories';
