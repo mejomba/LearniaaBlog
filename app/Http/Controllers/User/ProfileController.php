34f3c34eb8b98d59044410ba91815f34895d2f63
backslashteamgithub <learniaa@gmail.com>
2020-06-08 00:19:36 +0330

    profile   version  mreza 1.3

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -167,6 +167,18 @@ class ProfileController extends Controller
                     $profile->pic = 'profile_default.jpg' ;
                 }
     
+
+                if($profile->birthday != null && $profile->email != null && $profile->state != null  &&  $profile->address != null  &&  $profile->job != null  &&  $profile->favourite != null  &&  $profile->amount_time != null && $profile->area != null && $profile->pk_users != null)
+                {
+                    $profile->complete =  'yes'   ;
+
+                }  
+                else
+                {
+                    $profile->complete =  'no'  ;
+                }   
+
+
                 if($profile->save())
                 {
                     if(request()->password)
