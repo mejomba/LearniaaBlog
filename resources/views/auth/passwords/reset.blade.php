214575fdd4ac80502ccdb788b63447477095deac
Hossein-Joon <syd.hosseini.1998@gmail.com>
2020-07-01 17:34:30 +0430

    reset page changed to responsive

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -7,34 +7,30 @@
 
 @section('content')
 <div class="row" >
-<div class="col-lg-4 col-md-5 col-sm-6 col-9 offset-lg-1 offset-md-1 offset-sm-1 offset-1">
-<div class="card shadow border-0" style="border-radius:30px;">
-    <div class="card-header" style="background-color:#20C5BA ">
-                <div class="text-center"><h3 style="font-size: 22px">فراموشی رمز عبور</h3></div>
-              </div>
-
-    <div class="card-body px-2 py-3">
-
-          <form class="form" method="POST" action="{{route('reset.store')}}">
-              @csrf
-              <div class="form-group mt-4">
-                    <div class="input-group input-group-alternative">
-                      <div class="input-group-prepend">
-                      <img class="img-raised rounded-circle img-fluid"
-                      src="{{ asset('images/Template/phone_login.svg') }}" alt="Thumbnail Image" height="40px" width="40px">
-                      </div>
-                      <input name="username" id="username" type="text" class="form-control" placeholder="تلفن همراه">
-                    </div>
-                  </div>
-
-                  <div class="text-center mt-5 mb-4">
-                    <button type="submit" class="btn btn-primary">تایید  </button>
-                  </div>
-
-          </form>
-              </div>
-
-          </div>
+    <div class="col-lg-4 col-md-5 col-sm-6 col-8 offset-lg-1 offset-md-1 offset-sm-2 offset-2 text-center bg-white mt-5 shadow-lg" style="border-bottom-right-radius: 50px;border-bottom-left-radius: 50px;">
+        <div class="row">
+            <div class="col-12 p-0">
+                <h3 class="p-2" style="background-color:#20C5BA">فراموشی رمز عبور</h3>
+            </div>
         </div>
+
+        <form class="form" method="POST" action="{{route('reset.store')}}">
+
+            @csrf
+            <div class="form-group d-flex mt-3 pr-3 py-2">
+                <img class="rounded-circle mr-1"
+                     src="{{ asset('images/Template/phone_login.svg') }}" alt="Thumbnail Image" height="45px" width="45px">
+                <input name="username" id="username" type="text" class="form-control" placeholder=" تلفن همراه">
+
+            </div>
+
+            <input type="submit" class="btn btn-primary text-white text-center mb-4 mt-2" value="تایید">
+
+        </form>
     </div>
+
+    </div>
+
+
+
 @endsection
