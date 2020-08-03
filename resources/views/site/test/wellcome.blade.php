96b10a1cfe061d28ceb15373dbf80f3c14f40e23
Hossein-Joon <syd.hosseini.1998@gmail.com>
2020-08-03 19:56:11 +0430

    files changed

--- resources/views/site/test/wellcome.blade.php
+++ resources/views/site/test/wellcome.blade.php
@@ -5,5 +5,48 @@
     <meta name="keywords" content="نقشه راه لرنیا,چارت آموزشی لرنیا ,لرنیا آاکادمی">
 @endsection
 @section('content')
+<div class="startpage">
+    <input type="button" class="btn btn-primary" value="start" id="start" onclick="GoToEnterNamePage()">
+</div>
 
+<section class="enterName">
+    <div class="enterNameContent">
+        <div class="enterNameBody">
+            <div class="inputName m-5">
+                <input type="text" class="form-control w-100" placeholder="نام خود را وارد کنید..." id="inputName">
+            </div>
+            <div class="row btnStart">
+                <input type="button" class="btn btn-info col-4 offset-4 mb-4" id="btnStart" onclick="GoToRoadMap()" value="start">
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<script>
+    let id ;
+
+function GoToEnterNamePage() {
+    $.ajax({
+        url: 'http://127.0.0.1:8000/api/GenerateNewUuid',
+        data: {},
+        error: function() {
+            console.log('error')
+        },
+        dataType: 'jsonp',
+        success: function(data) {
+            JSON.parse(data);
+           conole.log(data) ;
+        },
+        type: 'GET'
+    });
+    $('.startpage').css('display','none');
+    $('.enterName').css('display','block');
+}
+
+function GoToRoadMap() {
+    window.open("roadmap.blade.php",'_blank');
+}
+</script>
 @endsection
