4222ec42b2ec43439ed690732a9a7810d14139f8
backslashTeamGithub <faq.backslash@gmail.com>
2020-08-03 19:51:42 +0430

    pdo edit other 19

--- resources/views/site/test/roadmap.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('site.Layouts.layout_main')
-@section('Head')
-    <title> لرنیا آکادمی | لرنیا </title>
-    <meta name="description" content="لرنیا آکادمی  | لرنیا ">
-    <meta name="keywords" content="نقشه راه لرنیا,چارت آموزشی لرنیا ,لرنیا آاکادمی">
-@endsection
-@section('content')
-
-@endsection
